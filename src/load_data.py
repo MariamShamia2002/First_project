15c8ecaa7caf77c76e2b597f0fb6173dbd2da24f
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-16 21:15:15 +0300

    load JSON file in load_data files

--- src/load_data.py
+++ src/load_data.py
@@ -5,7 +5,8 @@ csv_data=pd.read_csv('../data/raw/students_coffee_crisis.csv')
 print("CSV data :")
 print(csv_data.head())
 
-
-
-
+# Load JSON file
+json_data=pd.read_json('../data/raw/students_coffee_crisis.json', orient='records')
+print("JSON data :")
+print(json_data.head())
 
