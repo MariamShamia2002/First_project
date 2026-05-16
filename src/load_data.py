c40f92f8088b4d24af55b08c69114b2d09b31589
MariamShamia2002 <mariamshamia3@gmail.com>
2026-05-16 21:43:06 +0300

    fix file paths in load_data.py + load parquet file

--- src/load_data.py
+++ src/load_data.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
 # Load CSV file
-csv_data=pd.read_csv('../data/raw/students_coffee_crisis.csv')
+csv_data=pd.read_csv('data/raw/student_coffee_crisis.csv')
 print("CSV data :")
 print(csv_data.head())
 
 # Load JSON file
-json_data=pd.read_json('../data/raw/students_coffee_crisis.json', orient='records')
+json_data=pd.read_json('data/raw/student_coffee_crisis.json', orient='records')
 print("JSON data :")
 print(json_data.head())
 
 # Load Excel file
-excel_data=pd.read_excel('../data/raw/student_coffee_crisis.xlsx')
+excel_data=pd.read_excel('data/raw/student_coffee_crisis.xlsx')
 print("Excel data :")
 print(excel_data.head())
+
+#load parquet file
+parquet_data=pd.read_parquet('data/raw/student_coffee_crisis.parquet')
+print("Parquet data :")
+print(parquet_data.head())
